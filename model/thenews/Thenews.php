3fc7461ca9f50deac133dcff90f5ee1737a601ba
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-02-03 16:47:51 +0100

    ajout des attributs et des getters dans la page Thenews.php

--- model/thenews/Thenews.php
+++ model/thenews/Thenews.php
@@ -7,16 +7,54 @@ class Thenews
     private string $theUserLogin;
 
     // EXERCICE créez les autres attributs (noms des champs dans le table "thenews")
+    private id $idtheNews;
+    private string $theNewsTitle;
+    private string $theNewsText;
+    private string $theNewsDate;
+    private id $theUser_idtheUser;
 
 
     // EXERCICE créez le constructeur
-
+    // constructeur de thenews (Appelé lors de l'instanciation par new Article($param)), le tableau $param est obligatoire
+    public function __construct(Array $ma_param){
+        // hydratation de l'instance avec les valeurs dans le tableau
+        $this->hydrate($ma_param);
+    }
 
     // EXERCICE créez l'hydratateur
-
-
+    private function hydrate(Array $ma_datas){
+        foreach($ma_datas as $key => $ma_value){
+            $methodSetters = "set".ucfirst($key);
+            if(method_exists($this,$methodSetters)){
+                $this->$methodSetters($ma_value);
+            }
+        }
+    }
     // EXERCICE créez les getters et setters des attributs propre à cette table, n'oubliez pas de protéger les champs avec les setters !
+    public function getIdTheNews(): int
+    {
+        return $this->idtheNews;
+    }
 
+    public function getTheNewsTitle(): ?string
+    {
+        return $this->theNewsTitle;
+    }
+
+    public function getTheNewsText(): ?string
+    {
+        return $this->theNewsText;
+    }
+
+    public function getTheNewsDate(): ?string
+    {
+        return $this->theNewsDate;
+    }
+
+    public function getTheUser_idTheUser(): int 
+    {
+        return $this->theUser_idtheUser;
+    }
 
     // Getters et Setters utiles pour theUserLogin
     /**
@@ -40,5 +78,22 @@ class Thenews
         }else {
             $this->theUserLogin = $theUserLogin;
         }
+    }public function setIdTheNews(int $idtheNews): void
+    {
+        $this->idtheNews= $idtheNews;
     }
+
+    public function setTheNewsTitle(string $theNewsTitle): void
+    {
+         $title = strip_tags(trim($theNewsTitle));
+        if(empty($title)){
+            trigger_error("Votre titre ne peut être vide",E_USER_NOTICE);
+        }elseif (strlen($title)>180){
+            trigger_error("Votre titre ne peut pas dépasser 180 caractères",E_USER_NOTICE);
+        }else{
+            $this->theNewsTitle = $title;
+        }
+    }
+
+
 }
